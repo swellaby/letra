032f78e2421c6125ee0e546ae26cc4f244e598d7
calebcartwright <caleb.cartwright@outlook.com>
2019-07-24 21:14:44 -0500

    chore: add trove classifiers pre-publish

--- setup.py
+++ setup.py
@@ -17,4 +17,13 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/swellaby/letra",
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha"
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
