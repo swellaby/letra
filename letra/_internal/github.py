919fe18a622093cf25eaf484292f294283a0abf2
calebcartwright <caleb.cartwright@outlook.com>
2019-07-27 19:00:50 -0500

    feat: add support for github pat via environment variable

--- letra/_internal/github.py
+++ letra/_internal/github.py
@@ -1,14 +1,34 @@
 from letra import Label
 from .parser import extract_labels
 from .http_helpers import request_json
+from os import environ
 
 
 def get_base_label_api_url(owner: str, repository: str):
     return f"https://api.github.com/repos/{owner}/{repository}/labels"
 
 
-def get_headers(token: str = ""):
-    return {}
+def get_headers(token: str = "", authRequired: bool = False):
+    def create_auth_header(pat):
+        return {"Authorization": f"token {pat}"}
+
+    if token:
+        return create_auth_header(token)
+    else:
+        token = environ.get("LETRA_GITHUB_PAT")
+        if token:
+            return create_auth_header(token)
+        else:
+            if not authRequired:
+                return {}
+            raise ValueError(
+                (
+                    "GitHub PAT is required for operation, but no token "
+                    "was specified. Please provide a valid GitHub PAT for "
+                    "the `token` parameter, or set the LETRA_GITHUB_PAT "
+                    "environment variable with a valid GitHub PAT."
+                )
+            )
 
 
 async def get_labels_from_repository(
--- test/unit/internal/test_github.py
+++ test/unit/internal/test_github.py
@@ -13,6 +13,7 @@ request_json_mock_target = f"{sut_module_target}.request_json"
 extract_labels_mock_target = f"{sut_module_target}.extract_labels"
 get_base_url_mock_target = f"{sut_module_target}.get_base_label_api_url"
 get_headers_mock_target = f"{sut_module_target}.get_headers"
+environ_get_mock_target = f"{sut_module_target}.environ.get"
 exp_base_url = "https://api.github.com/repos/badges/shields/labels"
 exp_default_headers = {}
 
@@ -22,9 +23,55 @@ def test_get_base_label_api_url_returns_correct_url():
     assert url == exp_base_url
 
 
-def test_get_headers_returns_correct_headers():
+def get_auth_header(token):
+    return {"Authorization": f"token {token}"}
+
+
+def test_get_headers_returns_empty_headers_with_no_pat_and_optional_auth(
+    monkeypatch
+):
+    act_var = ""
+    exp_var = "LETRA_GITHUB_PAT"
+
+    def mock_environ_get(var_name):
+        nonlocal act_var
+        act_var = var_name
+        return None
+
+    monkeypatch.setattr(environ_get_mock_target, mock_environ_get)
     headers = get_headers()
     assert headers == exp_default_headers
+    assert act_var == exp_var
+
+
+def test_get_headers_raises_value_err_with_no_pat_and_required_auth(
+    monkeypatch
+):
+    exp = (
+        "GitHub PAT is required for operation, but no token was specified. "
+        "Please provide a valid GitHub PAT for the `token` parameter, "
+        "or set the LETRA_GITHUB_PAT environment variable with "
+        "a valid GitHub PAT."
+    )
+    monkeypatch.setattr(environ_get_mock_target, lambda x: None)
+    with pytest.raises(ValueError) as err:
+        get_headers(token="", authRequired=True)
+    assert str(err.value) == exp
+
+
+def test_get_headers_contains_auth_when_env_set(monkeypatch):
+    env_token = "abc123defXYZ"
+    monkeypatch.setattr(environ_get_mock_target, lambda x: env_token)
+    headers = get_headers()
+    assert headers == get_auth_header(env_token)
+
+
+def test_get_headers_uses_provided_token_over_env(monkeypatch):
+    env_token = "environmental"
+    exp_token = "123xyz987mno"
+    monkeypatch.setattr(environ_get_mock_target, lambda x: env_token)
+    headers = get_headers(token=exp_token)
+    assert headers == get_auth_header(exp_token)
 
 
 def stub_helper_functions(monkeypatch, url, headers):
